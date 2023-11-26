b449b0fd4a72c53661f74ea8ae99d1d465ad2adc
PheraxLemitige <97298319+PheraxLemitige@users.noreply.github.com>
2023-11-26 19:42:04 +0100

    ADD rotateCaméra

---
+++ AstroAdventure/Assets/_Script/cameraManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using System.Collections;
+
+public class cameraManager : MonoBehaviour {
+
+	public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+	public RotationAxes axes = RotationAxes.MouseXAndY;
+	public float sensitivityX = 5F;
+	public float sensitivityY = 5F;
+
+	public float minY = -60F;
+	public float maxY = 60F;
+
+	float rotationY = 0F;
+
+    void Start ()
+	{
+		
+	}
+
+	void Update ()
+	{
+		if (axes == RotationAxes.MouseXAndY)
+		{
+			float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+			
+			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+			rotationY = Mathf.Clamp (rotationY, minY, maxY);
+			
+			transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+		}
+		else if (axes == RotationAxes.MouseX)
+		{
+			transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+		}
+		else
+		{
+			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+			rotationY = Mathf.Clamp (rotationY, minY, maxY);
+			
+			transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+		}
+	}
+}
