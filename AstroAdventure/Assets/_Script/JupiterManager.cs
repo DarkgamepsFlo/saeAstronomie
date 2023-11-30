41dabe38edd531d4e71623c9e450db7598125e5e
Ogès Florian <94926129+DarkgamepsFlo@users.noreply.github.com>
2023-11-30 17:20:12 +0100

    Ajout des planètes
    Ajout de l'ensemble des planètes tournant autour du soleil

---
+++ AstroAdventure/Assets/_Script/JupiterManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JupiterManager : MonoBehaviour
+{
+    // VARIABLE : Elles vont contenir des données
+    // Représente les objets de la planète concerné et du soleil
+    public GameObject centreSun;
+    public GameObject centreEarth;
+    // Permet de contenir la vitesse de rotation
+    public float epsi;
+    // Permet de contenir les positions du soleil
+    public float positionSunx;
+    public float positionSuny;
+    public float positionSunz;
+    // VARIABLE : Elles vont permettre de modifier directement les données
+    // Permet de définir la taille de la terre
+    public float taillePlanete = 104.8f;
+    // Permet de définir l'ellipse de la planète
+    public float longueurEllipse = 1278f;
+    public float largeurEllipse = 1238f;
+    public float hauteurEllipse = 0f;
+    // Permet de définir la rotation de la planète
+    public float rotationx = 0.3f;
+    public float rotationy = -8f;
+    public float rotationz = 0f;
+    // Permet de définir la vitesse de la planète
+    public float vitesse = 0.135f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Permet de récupérer les objets représentant la planète concerné et du soleil
+        centreEarth = GameObject.Find("Jupiter");
+        centreSun = GameObject.Find("Sun");
+        // Permet de récupérer l'ensemble des positions x, y et z du soleil
+        positionSunx = centreSun.transform.position.x;
+        positionSuny = centreSun.transform.position.y;
+        positionSunz = centreSun.transform.position.z;
+        // Permet de redéfinir une nouvelle taille
+        centreEarth.transform.localScale = new Vector3(taillePlanete, taillePlanete, taillePlanete);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        epsi += vitesse * Time.deltaTime; // Ajuste la vitesse de rotation
+
+        float x = (positionSunx) + largeurEllipse * Mathf.Sin(epsi);
+        float y = (positionSuny) + hauteurEllipse * Mathf.Sin(epsi);
+        float z = (positionSunz) - longueurEllipse * Mathf.Cos(epsi);
+
+        transform.position = new Vector3(x, y, z);
+
+        transform.Rotate(new Vector3(rotationx, rotationy, rotationz) * Time.deltaTime);
+
+    }
+}
